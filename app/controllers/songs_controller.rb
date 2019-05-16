dcf79b4d34881785ad3b583fe946d8a77c89f9d6
Andrew Allen <asallen67@gmail.com>
2019-05-16 19:26:23 -0400

    enabled :sessions

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,6 +8,7 @@ class SongsController < ApplicationController
 		new_song = Song.new(params[:song])
 		new_song.artist = Artist.find_or_create_by(name: params[:artist_name]) 
 		new_song.save
+		session[:message] = "Successfully created song."
 		redirect "/songs/#{new_song.slug}"
 	end
 
@@ -17,18 +18,19 @@ class SongsController < ApplicationController
 
 	get '/songs/:slug' do
 		@song = Song.find_by_slug(params[:slug])
+		@temp = session[:message]
+		session[:message] = nil
 		erb :'songs/show'
 	end
 
 	patch '/songs/:slug' do
 		@song = Song.find_by_slug(params[:slug])
-		# binding.pry
 		params.delete(:_method)
 		params.delete(:slug)
-		@song.artist = Artist.find_or_create_by(name: params[:artist_name])
-		@song.genre_ids = params[:genre_ids] if params[:genre_ids] != nil
-		@song.save
-		# binding.pry
+
+		@song.update(genre_ids: params[:genre_ids], artist: Artist.find_or_create_by(name: params[:artist_name]))
+		
+		session[:message] = "Successfully updated song."
 		redirect "/songs/#{@song.slug}"
 	end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,11 @@ Genre.destroy_all
 SongGenre.destroy_all
 #...
 
-kanye = Artist.create(name: "Kanye West")
-kanye.songs.create(name: "Homecoming").genres.find_or_create_by(name: "Rap")
-kanye.songs.create(name: "Good Morning").genres.find_or_create_by(name: "Rap")
+# kanye = Artist.create(name: "Kanye West")
+# kanye.songs.create(name: "Homecoming").genres.find_or_create_by(name: "Rap")
+# kanye.songs.create(name: "Good Morning").genres.find_or_create_by(name: "Rap")
 
-swift = Artist.create(name: "Taylor Swift")
-swift.songs.create(name: "Love Story").genres.create(name: "Pop")
+# swift = Artist.create(name: "Taylor Swift")
+# swift.songs.create(name: "Love Story").genres.create(name: "Pop")
+
+LibraryParser.parse
